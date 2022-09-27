197bb7ede99462f196a544b9df723c709bb92873
Martín <magadiflo@gmail.com>
2022-09-27 17:47:12 -0500

    Implementando el componente JwtAuthenticationFilter

---
+++ src/main/java/com/magadiflo/gateway/security/JwtAuthenticationFilter.java
@@ -0,0 +1,33 @@
+package com.magadiflo.gateway.security;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtAuthenticationFilter implements WebFilter{
+	
+	private final ReactiveAuthenticationManager reactiveAuthenticationManager;
+	
+	public JwtAuthenticationFilter(ReactiveAuthenticationManager reactiveAuthenticationManager) {
+		this.reactiveAuthenticationManager = reactiveAuthenticationManager;
+	}
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+		return Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION))
+				.filter(authHeader -> authHeader.startsWith("Bearer "))
+				.switchIfEmpty(chain.filter(exchange).then(Mono.empty()))
+				.map(token -> token.replace("Bearer ", ""))
+				.flatMap(token -> this.reactiveAuthenticationManager.authenticate(new UsernamePasswordAuthenticationToken(null, token)))
+				.flatMap(authentication -> chain.filter(exchange).contextWrite(ReactiveSecurityContextHolder.withAuthentication(authentication)));
+	}
+
+}
