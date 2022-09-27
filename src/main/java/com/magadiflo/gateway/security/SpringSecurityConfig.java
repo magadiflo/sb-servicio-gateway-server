b8da564c9cafe9ca80bbbdf00876e1430d44b2a5
Martín <magadiflo@gmail.com>
2022-09-26 20:02:25 -0500

    Dando reglas de seguridad a nuestras rutas de Spring Cloud Gateway

--- src/main/java/com/magadiflo/gateway/security/SpringSecurityConfig.java
+++ src/main/java/com/magadiflo/gateway/security/SpringSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.magadiflo.gateway.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -11,6 +12,10 @@ public class SpringSecurityConfig {
 	@Bean
 	public SecurityWebFilterChain configure(ServerHttpSecurity http) {
 		return http.authorizeExchange()
+				.pathMatchers("/api/security/oauth/**").permitAll()
+				.pathMatchers(HttpMethod.GET, "/api/productos/productos", "/api/items/items", "/api/usuarios/usuarios", "/api/items/items/producto/{id}/cantidad/{cantidad}", "/api/productos/productos/{id}").permitAll()
+				.pathMatchers(HttpMethod.GET, "/api/usuarios/usuarios/{id}").hasAnyRole("ADMIN", "USER")
+				.pathMatchers("/api/productos/**", "/api/items/**", "/api/usuarios/**").hasRole("ADMIN")
 				.anyExchange().authenticated()
 				.and()
 				.csrf().disable().build();
