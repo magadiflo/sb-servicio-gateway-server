381f6c307d9c9a6057ba5b8bea63c62a13ec46c1
Martín <magadiflo@gmail.com>
2022-09-27 18:21:30 -0500

    Registrando filtro JwtAuthenticationFilter en la configuración de Spring Security

--- src/main/java/com/magadiflo/gateway/security/SpringSecurityConfig.java
+++ src/main/java/com/magadiflo/gateway/security/SpringSecurityConfig.java
@@ -3,11 +3,18 @@ package com.magadiflo.gateway.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
 public class SpringSecurityConfig {
+	
+	private final JwtAuthenticationFilter authenticationFilter;
+	
+	public SpringSecurityConfig(JwtAuthenticationFilter authenticationFilter) {
+		this.authenticationFilter = authenticationFilter;
+	}
 
 	@Bean
 	public SecurityWebFilterChain configure(ServerHttpSecurity http) {
@@ -18,6 +25,7 @@ public class SpringSecurityConfig {
 				.pathMatchers("/api/productos/**", "/api/items/**", "/api/usuarios/**").hasRole("ADMIN")
 				.anyExchange().authenticated()
 				.and()
+				.addFilterAt(this.authenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
 				.csrf().disable().build();
 	}
 
